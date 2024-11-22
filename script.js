ef7b2a448a7546e4057432c44e380543a121b671
Kay Who Codes <68508154+kay-who-codes@users.noreply.github.com>
2024-11-22 19:54:06 +0000

    Added more of everything

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 // script.js
 
-const genres = ["Pop", "Rock", "Jazz", "Hip-Hop", "Classical", "EDM", "Country", "Blues"];
-const structures = [
-  "I, IV, V, I",
-  "I, V, vi, IV",
-  "ii, V, I",
-  "I, vi, IV, V",
-  "I, IV, I, V"
+const genres = [
+  "Pop", "Rock", "Jazz", "Hip-Hop", "Orchestral", "EDM", "Country", "Blues", "Soul", "Funk", "Disco", "Folk", "Metal", "Grunge", "Punk",
+  "R&B", "Trap", "House", "Techno", "Ambient", "Latin", "Garage", "Anthem", "Electroswing", "Instrumental", "Techno"
 ];
-const vibes = ["Fun", "Angry", "Silly", "Chill", "Romantic", "Energetic", "Dark"];
 
+const structures = [
+  "I, IV, V, I", "I, V, vi, IV", "ii, V, I", "I, vi, IV, V", "I, IV, I, V",
+  "I, IV, V", "ii, V, vi, IV", "I, vi, ii, V", "I, IV, V, vi", "I, V, I",
+  "vi, IV, V, I", "I, V, IV, I", "I, ii, IV, V", "I, IV, V, IV", 
+  "ii, IV, V, I", "I, IV, V, vi, IV", "I, V, ii, vi"
+];
+
+const vibes = [
+  "Fun", "Angry", "Silly", "Chill", "Energetic", "Dark", "Melancholic", 
+  "Dreamy", "Quirky", "Motivational", "Mysterious", "Peaceful", 
+  "Upbeat", "Aggressive", "Hopeful", "Enlightening", "Experimental"
+];
 function getRandomItem(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
